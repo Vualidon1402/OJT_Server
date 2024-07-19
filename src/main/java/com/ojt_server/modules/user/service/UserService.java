a0515de3a8a13f7d8ac51cdcb50547b9a75147a2
thienpxc <thienpxc@gmail.com>
2024-07-19 18:06:24 +0700

    feature/api-block-unlock

--- src/main/java/com/ojt_server/modules/user/service/UserService.java
+++ src/main/java/com/ojt_server/modules/user/service/UserService.java
@@ -6,20 +6,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+
+//    findById
+
     //hiển thị danh sách user
     public List<UserModel> findUsers(){
-        try{
-            return userRepository.findAll();
-        }catch (Exception e){
-            e.printStackTrace();
+        return userRepository.findAll();
+    }
+    //thay đổi trạng thái status
+    public UserModel update(Long id){
+        Optional<UserModel> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            UserModel user = optionalUser.get();
+            user.setStatus(!user.isStatus()); // Flip the status
+            return userRepository.save(user);
+        } else {
+            System.out.println("User not found with id: " + id);
             return null;
         }
+
+
     }
+
+
+
+
 
 }
