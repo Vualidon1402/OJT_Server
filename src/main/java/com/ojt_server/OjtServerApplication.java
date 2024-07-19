e73983b767c63276c4e2a6b64a427ee84ff55c41
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-07-19 08:48:13 -0700

    first commit

---
+++ src/main/java/com/ojt_server/OjtServerApplication.java
@@ -0,0 +1,13 @@
+package com.ojt_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjtServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OjtServerApplication.class, args);
+    }
+
+}
