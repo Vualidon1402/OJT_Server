c6368bc40fe60061622154ccae1d36d934ce7970
Vualidon1402 <nguyendanhkien1402@gmai.com>
2024-07-20 15:47:52 -0700

    after rebase conflict CategoryManager api-add-edit-delete

--- src/main/java/com/ojt_server/modules/category/controller/CategoryController.java
+++ src/main/java/com/ojt_server/modules/category/controller/CategoryController.java
@@ -1,25 +1,40 @@
 package com.ojt_server.modules.category.controller;
 
 import com.ojt_server.modules.category.CategoryModel;
-import com.ojt_server.modules.category.service.ICategoryService;
+import com.ojt_server.modules.category.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/category")
-@CrossOrigin("*")
 public class CategoryController {
 
     @Autowired
-    private ICategoryService categoryService;
+    private CategoryService categoryService;
 
     @GetMapping("/findAll")
     public List<CategoryModel> getAllCategory() {
-        return categoryService.findAll();
+        return categoryService.findCategories();
     }
+
+    // Add new category
+    @PostMapping("/add")
+    public CategoryModel addCategory(@RequestBody CategoryModel category){
+        return categoryService.addCategory(category);
+    }
+
+    // Update category
+    @PutMapping("/update")
+    public CategoryModel updateCategory (@RequestBody CategoryModel category){
+        return categoryService.updateCategory(category);
+    }
+
+    // Update category status instead of deleting
+    @DeleteMapping("/delete/{id}")
+    public void deleteCategory(@PathVariable Long id){
+        categoryService.deleteCategory(id);
+    }
+
 }
---
+++ src/main/java/com/ojt_server/modules/category/service/CategoryService.java
@@ -0,0 +1,55 @@
+package com.ojt_server.modules.category.service;
+
+import com.ojt_server.modules.category.CategoryModel;
+import com.ojt_server.modules.category.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<CategoryModel> findCategories() {
+        try {
+            return categoryRepository.findAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Add new category
+    public CategoryModel addCategory(CategoryModel category) {
+        try {
+            return categoryRepository.save(category);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Update category
+    public CategoryModel updateCategory(CategoryModel category) {
+        try {
+            return categoryRepository.save(category);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // Update category status instead of deleting
+    public void deleteCategory(Long id) {
+        try {
+            categoryRepository.findById(id).ifPresent(category -> {
+                category.setStatus(false); // Set status to false
+                categoryRepository.save(category); // Save the updated category
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
